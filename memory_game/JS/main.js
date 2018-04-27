bbbfbcf1f03dd94a1e68a154edfc64027c4777f2
Michael Craig <michaelcraig@Michaels-MBP.attlocal.net>
2018-04-27 14:43:32 -0400

    created game logic

---
+++ memory_game/JS/main.js
@@ -0,0 +1,61 @@
+
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}
+];
+
+var cardsInPlay = [];
+// var score = 0;
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+		/*
+		score += 1;
+		document.getElementById('score')
+		*/
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	this.setAttribute('class', 'clicked');
+	cardsInPlay.push(cards[cardId].rank);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();	
+		cardsInPlay = [];
+	}
+}
+
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
